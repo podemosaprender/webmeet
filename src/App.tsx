5768a998a3d03e8a332f7c5e29c8a8c3f7be9e28
devel@mauriciocap.com <devel@mauriciocap.com>
2024-01-03 11:02:57 -0500

    OK: primereact + style

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/lara-dark-purple/theme.css'
+//SEE: https://primereact.org/configuration/
+
 import { useState } from 'react'
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
+	const [date, setDate] = useState(null);
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+	return (
+		<PrimeReactProvider>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<Button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</Button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+				<Calendar value={date} onChange={(e) => setDate(e.value)} />
+			</div>
+		</PrimeReactProvider>
+	)
 }
 
 export default App
